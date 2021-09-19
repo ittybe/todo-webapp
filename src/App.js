a150eed59fcee68a9ccbd4eee0d091e19680dd96
michael <ittybemain@gmail.com>
2021-09-19 19:10:34 +0300

    set up dark mode for background images, header styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 class App extends React.Component {
     render() {
         return (
+            
             <div className="testing rounded">something something</div>
         )
     }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'img-light-mb': "url('./assets/bg-mobile-light.jpg')",
+        'img-dark-mb': "url('./assets/bg-mobile-dark.jpg')",
+        'img-light-desk': "url('./assets/bg-desktop-light.jpg')",
+        'img-dark-desk': "url('./assets/bg-desktop-dark.jpg')",
+        "moon-sign": "url('./assets/icon-moon.svg')",
+        "sun-sign": "url('./assets/icon-sun.svg')",
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundImage: ["dark"]
+    },
   },
   plugins: [],
 }
