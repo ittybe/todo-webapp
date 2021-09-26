31413d1e3575a1c3ff3822181a4b93e2198bf578
michael <ittybemain@gmail.com>
2021-09-26 19:47:14 +0300

    some changes

--- src/Task.js
+++ src/Task.js
@@ -11,27 +11,25 @@ export default class Task extends React.Component {
 
     render() {
         return (
-            <div className="flex flex-row items-start">
+            <div className="task">
                 <div className={
                     `
                     check
-                    hover:bg-check-gradient 
-                    ` + (this.props.task.isActive ? " bg-l-dark-grayish-blue " : " bg-check-gradient ")
+                    dark:d-check
+                    ` + (this.props.task.isActive ? "bg-l-dark-grayish-blue " : " bg-check-gradient ")
                 }
                 >
                     <button onClick={() => this.markTask()} className={
-                    `
-                    bg-white
-                    ` + (this.props.task.isActive ? "  " : " bg-check-gradient ")   
+                    (this.props.task.isActive ? "  " : " bg-check-gradient ")   
                     }
                     >
-                        <div className={" icon  " + (this.props.task.isActive? "": "bg-icon-check")}></div>
+                        <div className={"icon  " + (this.props.task.isActive? "": "bg-icon-check")}></div>
                     </button>
                 </div>
                
                 <button onClick={() => this.markTask()} className="flex-grow text-left self-center">{this.props.task.taskBody}</button>
-                <button onClick={() => this.removeTask()} className="m-4 p-2">
-                    <div className="icon bg-icon-cross"></div>
+                <button onClick={() => this.removeTask()} className="cross ">
+                    <div className="icon"></div>
                 </button>
             </div>
         ) 
